15e55d9b850fc875892e847c5c2cb26defe63d0f
khachatryanani <anikhachatryan94@gmail.com>
2020-11-19 04:25:25 -0800

    adding main Bank Application file

---
+++ BankApplication.cpp
@@ -0,0 +1,59 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "Banking.cpp"
+
+using namespace std;
+
+int main()
+{
+	Bank b = Bank();
+	char choice;	
+	do 
+	{
+		choice = b.GetUserChoice();
+		switch (choice)
+		{
+		case '1':
+			system("cls");
+			b.UserToCreateAccount();
+			system("cls");
+			break;
+		case '2':
+			system("cls");
+			b.UserToUpdateAccountInfo();
+			system("cls");
+			break;
+		case '3':
+			system("cls");
+			b.UserToPerformTransaction();
+			system("cls");
+			break;
+		case '4':
+			system("cls");
+			b.UserToCheckAccountInfo();
+			system("cls");
+			break;
+		case '5':
+			system("cls");
+			b.UserToRemoveAccount();
+			system("cls");
+			break;
+		case '6':
+			system("cls");
+			b.UserToSeeCustomersList();
+			system("cls");
+			break;
+		case'7':
+			system("cls");
+			cout << "You've exited the application!";
+			break;
+		default:
+			system("cls");
+			cout << "Please choose from the options below\n";
+		}
+	} 
+	while (choice != '7');
+}
+
